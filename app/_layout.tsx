6107cb98566810f09498ccff77b5ad61766e84a3
raj248 <shashankraj007281@gmail.com>
2025-10-24 12:47:57 +0530

    no alert on share intent crash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout() {
   useEffect(() => {
     if (error) {
       console.error('Share Intent Error:', error);
-      alert(`Error processing share: ${error}`);
+      // alert(`Error processing share: ${error}`);
       return;
     }
 
